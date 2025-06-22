dab6e2d865ecbb83ee747688df2305da32d42d2b
muhammedAslam <aslamap707@gmail.com>
2025-06-22 15:22:55 +0530

    deploy v_7

--- front-end/src/pages/admin/componets/AdminLayout.tsx
+++ front-end/src/pages/admin/componets/AdminLayout.tsx
@@ -6,7 +6,6 @@ interface AdminLayoutProps {
   children: React.ReactNode
 }
 
-
 export const AdminLayout = ({ children }: AdminLayoutProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
