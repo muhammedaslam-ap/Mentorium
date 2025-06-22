f69c26110c8f3077a4c39652fba1de7fd3d8c89d
muhammedAslam <aslamap707@gmail.com>
2025-06-22 21:27:37 +0530

    deploy v_11

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -36,7 +36,7 @@ cloudinary.config({
 });
 
 const corsOptions = {
-  origin: ["https://mentorium.aslamap.tech"], 
+  origin: [process.env.FRONTEND_URL || "http://localhost:5173"], 
   credentials: true,
   methods: ["GET", "POST", "OPTIONS", "PATCH", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
